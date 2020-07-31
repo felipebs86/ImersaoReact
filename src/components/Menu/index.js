b54c810bb88ae9e6d39e6792e57268a1b9e835c2
Felipe Bahiense <felipebahiensesantos@gmail.com>
2020-07-31 19:02:20 -0300

    segunda aula

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import logo from '../../assets/img/logo.png'
-import './Menu.css'
+import './Menu.scss'
 import Button from '../Button';
 
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" src={logo} alt="FBSFLIX Logo"></img>
-            </a>
+            </Link>
 
-            <Button as="a" className="ButtonLink" href="/">
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
                 Novo video
             </Button>
 
